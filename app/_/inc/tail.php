124a16bcc8c3d23e2c00fc4215b38ead26cc5efa
Siphumelelise Ntshwenyese <sjayjay005@gmail.com>
2015-06-02 13:37:48 +0200

    New changes

---
+++ app/_/inc/tail.php
@@ -0,0 +1,13 @@
+
+
+        <!-- build:js(app) /_/js/main.js -->
+        <script src="/_/bower_components/jquery/jquery.js"></script>
+        <script src="/_/bower_components/html5-boilerplate/js/plugins.js"></script>
+        <script src="/_/js/functions.js"></script>
+        <script src="/_/js/validation.js"></script>
+        <!-- endbuild -->
+
+
+<!-- build:remove:dist -->
+<script src="//localhost:1025/livereload.js"></script>
+<!-- /build -->
